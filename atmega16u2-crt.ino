af5a286c0c9ee7243cc445c47fb6a2d0eb5f8220
PrinceOfPuppers <joshuamcpherson5@gmail.com>
2024-05-16 14:07:33 -0400

    text scrolling

--- atmega16u2-crt.ino
+++ atmega16u2-crt.ino
@@ -1,15 +1,103 @@
+#include "HID-Project.h"
+#include "fonts/processed/pet.h"
+
 #include "drawing.h"
-#include "string.h"
+#include <string.h>
+#include <stdint.h>
 
-//uint8_t rawhidData[CHUNK_BYTE_SIZE];
 
+#define LINE_SPACING 1
+#define CHARACTER_SPACING 0
+#define MAX_LINES (255/(CHAR_HEIGHT + LINE_SPACING))
+
+#define RAW_HID_BUFFER_SIZE 16
+uint8_t rawhidData[RAW_HID_BUFFER_SIZE];
+
+
+#define SCREEN_BUFFER_SIZE 128
+uint8_t screen_buffer[SCREEN_BUFFER_SIZE];
+uint8_t screen_write_head = 0;
+
+// reads oldest to newest
+#define read_from_screen_buffer(v)  screen_buffer[(screen_write_head + v)%SCREEN_BUFFER_SIZE]
+
+uint8_t num_newlines = 0;
+void write_to_screen_buffer(char val){
+    num_newlines += (val == '\n') - (screen_buffer[screen_write_head] == '\n');
+    screen_buffer[screen_write_head] = val; 
+    screen_write_head += 1; 
+    screen_write_head %= SCREEN_BUFFER_SIZE;
+}
+
+const char s[] = "this is a\nreally\nlong\nsilly\nstring\nabout\nnothing\nin\nparticular\n";
 void setup(){
+    memset(screen_buffer, 0, SCREEN_BUFFER_SIZE);
+    RawHID.begin(rawhidData, sizeof(rawhidData));
     setup_drawing();
+
+    for(int i = 0; i < sizeof(s); i++){
+        write_to_screen_buffer(s[i]);
+    }
 }
 
 
+void write_to_screen(){
+    noInterrupts();
+    uint16_t x = 0;
+    uint16_t y = 0;
+    uint8_t line = 0; 
+    for(int i = 0; i < SCREEN_BUFFER_SIZE; i++){
+        //char c = read_from_screen_buffer(i);
+        char c = read_from_screen_buffer(i);
+
+        // ignore lines off screen
+        if(num_newlines - line > MAX_LINES-1){
+            if(c == '\n'){
+                line+=1;
+            }
+            continue;
+        }
+        if(c == '\n'){
+            x = 0;
+            y += CHAR_HEIGHT + LINE_SPACING;
+            continue;
+        }
+
+        if(x + MAX_CHAR_WIDTH >= 255){
+            continue;
+        }
+
+        x += write_char(x, y, c) + CHARACTER_SPACING;
+
+    }
+    write_char(x, y, '_');
+    interrupts();
+}
+
+void test(){
+    write_to_screen();
+    //write(s, len);
+}
+
+int index = 0;
+int counter = 0;
 void loop(){
-    const char *s = " !\"#$%&\'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`abcdefghijklmnopqrstuvwxyz{|}~";
-    write(s, strlen(s));
+    if(counter%10 == 0){
+        write_to_screen_buffer(s[index%sizeof(s)]);
+            index+=1;
+    }
+    counter+=1;
+
+    test();
+    return;
+    /*
+    int bytesAvailable = RawHID.available();
+    while(bytesAvailable){
+        write_to_screen_buffer(RawHID.read());
+        bytesAvailable--;
+    }
+    write_to_screen();
+    */
+
     return;
 }
